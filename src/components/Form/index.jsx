c372c27080c6543d17f687be48108e72da1bbc53
leninner <mazabandalenin180@gmail.com>
2021-12-29 21:44:09 -0500

    Update

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -13,13 +13,15 @@ export const FormComponent = () => {
   const cargo = useInputValue('');
   const email = useInputValue('');
   const telefono = useInputValue('');
-  const experiencias = useInputValue('');
+  const empresa = useInputValue('');
+  const descripcion = useInputValue('');
 
   const [experiencies, setExperiencies] = useState([]);
 
   const handleAddExperiencia = () => {
-    setExperiencies([...experiencies, experiencias]);
-    experiencias.onChange({ target: { value: '' } });
+    setExperiencies([...experiencies, { empresa, descripcion }]);
+    empresa.onChange({ target: { value: '' } });
+    descripcion.onChange({ target: { value: '' } });
   };
 
   const handleDelete = (index) => {
@@ -56,15 +58,65 @@ export const FormComponent = () => {
         <Input type='text' placeholder='Teléfono' {...telefono} id='tel' />
       </Label>
       <Label htmlFor=''>
-        <Input type='text' placeholder='Experiencia' {...experiencias} />
+        Experiencia:
+        <Items>
+          <div>
+            <Label htmlFor='empresa'>
+              Empresa:
+              <Input type='text' placeholder='Empresa' {...empresa} id='empresa' />
+            </Label>
+            <Label htmlFor='empresa'>
+              Descripción:
+              <Input type='text' placeholder='Descripción' {...descripcion} />
+            </Label>
+          </div>
 
+          <div>
+            <Label>
+              Fecha Inicio:
+              <Input type='date' name='' id='' />
+            </Label>
+
+            <Label>
+              Fecha Fin:
+              <Input type='date' name='' id='' />
+            </Label>
+          </div>
+        </Items>
         {experiencies.map((experiencia, index) => {
+          const { empresa, descripcion } = experiencia;
+
+          console.log(empresa, descripcion);
+
           return (
             <Items key={index}>
-              <Input type='text' placeholder='Experiencia' {...experiencia} key={index} disabled={edit} />
-              {console.log(experiencia.value)}
-              <AiFillEdit onClick={handleEdit} size='32' />
-              <AiFillDelete onClick={() => handleDelete(index)} size='32' />
+              <div>
+                <Label htmlFor='empresa'>
+                  Empresa:
+                  <Input type='text' placeholder='Empresa' {...empresa} id='empresa' disabled={edit} />
+                </Label>
+                <Label htmlFor='empresa'>
+                  Descripción:
+                  <Input type='text' placeholder='Descripción' {...descripcion} disabled={edit} />
+                </Label>
+              </div>
+
+              <div>
+                <Label>
+                  Fecha Inicio:
+                  <Input type='date' name='' id='' disabled={edit} />
+                </Label>
+
+                <Label>
+                  Fecha Fin:
+                  <Input type='date' name='' id='' disabled={edit} />
+                </Label>
+              </div>
+
+              <div className='icons'>
+                <AiFillDelete onClick={() => handleDelete(index)} size='25' />
+                <AiFillEdit onClick={handleEdit} size='25' />
+              </div>
             </Items>
           );
         })}
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -15,7 +15,7 @@ export const Form = styled.form`
 `;
 
 export const Input = styled.input`
-  width: 100%;
+  width: 95%;
   padding: 10px;
   border: 1px solid #ddd;
   border-radius: 5px;
@@ -25,6 +25,12 @@ export const Input = styled.input`
   &:focus {
     border-color: #4d85ee;
     box-shadow: 0 0 10px rgba(77, 133, 238, 0.5);
+    outline: none;
+    background-color: rgba(77, 133, 238, 0.2);
+  }
+
+  &[disabled] {
+    background-color: #e2ece9;
   }
 `;
 
@@ -49,5 +55,19 @@ export const Button = styled.button`
 export const Items = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   width: 100%;
+  max-width: 500px;
+  margin: 10px auto;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border: 1px solid #ddd;
+  position: relative;
+
+  .icons {
+    position: absolute;
+    bottom: 0;
+    right: calc(50% - 25px);
+  }
 `;
