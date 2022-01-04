7a38e30dc0e6d2035fa38468f9458c4d55ba5ab2
leninner <mazabandalenin180@gmail.com>
2022-01-04 18:35:34 -0500

    Update UI

--- src/components/FormComponents/FormMainInfo/index.jsx
+++ src/components/FormComponents/FormMainInfo/index.jsx
@@ -1,14 +1,21 @@
 import { FormInputs } from '../FormInputs';
+import { TextArea } from '../FormExperience/styles';
+import { Label } from '../FormInputs/styles';
 
 export const FormMainInfo = () => {
   return (
     <>
       <FormInputs type='text' name='name' placeholder='Name' />
       <FormInputs type='text' name='apellido' placeholder='Apellido' />
+      <Label>
+        Descríbete:
+        <TextArea name='description' placeholder='¿Quién eres?' />
+      </Label>
       <FormInputs type='email' name='email' placeholder='Email' />
       <FormInputs type='tel' name='tel' placeholder='Phone' />
       <FormInputs type='text' name='linkedIn' placeholder='LinkedIn' />
       <FormInputs type='text' name='github' placeholder='Github' />
+      <FormInputs type='text' name='twitter' placeholder='Twitter' />
     </>
   );
 };
