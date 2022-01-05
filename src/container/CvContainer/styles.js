2a784cf55201d7f9078174e767ecd83bff27929a
leninner <mazabandalenin180@gmail.com>
2022-01-05 14:58:09 -0500

    Update UI

--- src/container/CvContainer/styles.js
+++ src/container/CvContainer/styles.js
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 50%;
-  padding: 10px;
+  padding: 10px 0 0 10px;
 
   @media (max-width: 768px) {
     width: 100%;
-    padding: 0;
   }
 `;
--- src/container/FormContainer/styles.js
+++ src/container/FormContainer/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 50%;
   padding: 10px;
+  border-right: 1px solid grey;
 
   @media (max-width: 768px) {
     width: 100%;
