6b220bbd442b1af1fc5ce6966b51d140a54786fc
leninner <mazabandalenin180@gmail.com>
2022-01-07 11:48:32 -0500

    Actualizo la forma de editar los campos

--- src/components/FormComponents/FormMainInfo/index.jsx
+++ src/components/FormComponents/FormMainInfo/index.jsx
@@ -7,7 +7,6 @@ import { FormContext } from '../../../context/FormContext';
 export const FormMainInfo = () => {
   const { generalState, setGeneralState } = useContext(FormContext);
   const [mainInfoValues, setMainInfoValues] = useState({});
-  const [isDisabled, setIsDisabled] = useState(false);
   const [edit, setEdit] = useState(false);
 
   const handleChange = (e) => {
@@ -20,115 +19,91 @@ export const FormMainInfo = () => {
       ...generalState,
       mainInfoValues: mainInfoValues,
     });
-    setIsDisabled(true);
+    setEdit(true);
   };
 
-  const handleEdit = () => {
-    setEdit(true);
-    setIsDisabled(false);
-  };
+  const handleEdit = () => setEdit(false);
 
   return (
     <>
-      {!edit ? (
-        <>
-          <FormInputs type='text' name='name' placeholder='Name' onChange={handleChange} disabled={isDisabled} />
-          <FormInputs
-            type='text'
-            name='lastName'
-            placeholder='Apellido'
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <FormInputs type='text' name='cargo' placeholder='Cargo' onChange={handleChange} disabled={isDisabled} />
-          <Label>
-            Descríbete:
-            <TextArea name='description' placeholder='¿Quién eres?' onChange={handleChange} disabled={isDisabled} />
-          </Label>
-          <FormInputs type='email' name='email' placeholder='Email' onChange={handleChange} disabled={isDisabled} />
-          <FormInputs type='tel' name='tel' placeholder='Phone' onChange={handleChange} disabled={isDisabled} />
-          <FormInputs
-            type='text'
-            name='linkedIn'
-            placeholder='LinkedIn'
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <FormInputs type='text' name='github' placeholder='Github' onChange={handleChange} disabled={isDisabled} />
-          <FormInputs type='text' name='twitter' placeholder='Twitter' onChange={handleChange} disabled={isDisabled} />
-          {isDisabled ? (
-            <FormInputs type='button' name='button' value='Editar' onClick={handleEdit} />
-          ) : (
-            <FormInputs type='button' name='button' value='Aceptar' onClick={handleSubmit} />
-          )}
-        </>
+      <FormInputs
+        type='text'
+        name='name'
+        placeholder='Nombre'
+        value={mainInfoValues.name || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      <FormInputs
+        type='text'
+        name='lastName'
+        placeholder='Apellido'
+        value={mainInfoValues.lastName || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      <FormInputs
+        type='text'
+        name='cargo'
+        placeholder='Cargo'
+        value={mainInfoValues.cargo || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      <Label>
+        Descríbete:
+        <TextArea
+          name='description'
+          placeholder='Describe tu perfil'
+          value={mainInfoValues.description || ''}
+          onChange={handleChange}
+          disabled={edit}
+        />
+      </Label>
+      <FormInputs
+        type='email'
+        name='email'
+        placeholder='Email'
+        value={mainInfoValues.email || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      <FormInputs
+        type='tel'
+        name='tel'
+        placeholder='Teléfono'
+        value={mainInfoValues.tel || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      <FormInputs
+        type='text'
+        name='linkedIn'
+        placeholder='LinkedIn'
+        value={mainInfoValues.linkedIn || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      <FormInputs
+        type='text'
+        name='github'
+        placeholder='Github'
+        value={mainInfoValues.github || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      <FormInputs
+        type='text'
+        name='twitter'
+        placeholder='Twitter'
+        value={mainInfoValues.twitter || ''}
+        onChange={handleChange}
+        disabled={edit}
+      />
+      {edit ? (
+        <FormInputs type='button' name='button' value='Editar' onClick={handleEdit} />
       ) : (
-        <>
-          <FormInputs
-            type='text'
-            name='name'
-            value={mainInfoValues.name}
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <FormInputs
-            type='text'
-            name='lastName'
-            value={mainInfoValues.lastName}
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <FormInputs
-            type='text'
-            name='cargo'
-            value={mainInfoValues.cargo}
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <Label>
-            Descríbete:
-            <TextArea
-              name='description'
-              value={mainInfoValues.description}
-              onChange={handleChange}
-              disabled={isDisabled}
-            />
-          </Label>
-          <FormInputs
-            type='email'
-            name='email'
-            value={mainInfoValues.email}
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <FormInputs type='tel' name='tel' value={mainInfoValues.tel} onChange={handleChange} disabled={isDisabled} />
-          <FormInputs
-            type='text'
-            name='linkedIn'
-            value={mainInfoValues.linkedIn}
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <FormInputs
-            type='text'
-            name='github'
-            value={mainInfoValues.github}
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          <FormInputs
-            type='text'
-            name='twitter'
-            value={mainInfoValues.twitter}
-            onChange={handleChange}
-            disabled={isDisabled}
-          />
-          {isDisabled ? (
-            <FormInputs type='button' name='button' value='Editar' onClick={handleEdit} />
-          ) : (
-            <FormInputs type='button' name='button' value='Aceptar' onClick={handleSubmit} />
-          )}
-        </>
+        <FormInputs type='button' name='button' value='Aceptar' onClick={handleSubmit} />
       )}
     </>
   );
